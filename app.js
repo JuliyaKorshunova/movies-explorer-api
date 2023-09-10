2168f99e1d7e7618c23d00ad425f68855e3a24e1
JuliyaKorshunova <121635276+JuliyaKorshunova@users.noreply.github.com>
2023-09-11 01:34:13 +0300

    Diplom 4 test

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const rateLimit = require('express-rate-limit');
 const errorHandler = require('./middlewares/error_handler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3005, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
 const app = express();
 
